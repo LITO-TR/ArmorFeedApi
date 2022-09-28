550e5d4465ea65e6495b0a9f36563d44dee6e70e
Nilton Torres <niltontorresrojas8@gmail.com>
2022-09-28 03:02:08 -0500

    deploy

---
+++ ArmorFeedApi/Customers/Domain/Repositories/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using ArmorFeedApi.Customers.Domain.Models;
+
+namespace ArmorFeedApi.Customers.Domain.Repositories;
+
+public interface ICustomerRepository
+{
+    Task<IEnumerable<Customer>> ListAsync();
+    Task AddAsync(Customer customer);
+    Task<Customer> FindByIdAsync(int id);
+    void Update(Customer customer);
+    void Remove(Customer customer);
+}
---
+++ ArmorFeedApi/Vehicles/Domain/Services/Communication/VehicleResponse.cs
@@ -0,0 +1,15 @@
+﻿using ArmorFeedApi.Shared.Domain.Services.Communication;
+using ArmorFeedApi.Vehicles.Domain.Models;
+
+namespace ArmorFeedApi.Vehicles.Domain.Services.Communication;
+
+public class VehicleResponse : BaseResponse<Vehicle>
+{
+    public VehicleResponse(Vehicle resource) : base(resource)
+    {
+    }
+
+    public VehicleResponse(string message) : base(message)
+    {
+    }
+}
